716544c7ee3c1c55152e7aa16456f726ae3d14be
mariam-farrukh <50977034+mariam-farrukh@users.noreply.github.com>
2019-09-09 14:38:09 -0500

    Finished api requests with db

--- posts/post-router.js
+++ posts/post-router.js
@@ -9,8 +9,9 @@ router.get('/', (req, res) => {
     // get data from database and return it to the client
     // select * from posts
     //in knex documentation this is knex.select, but we just rename it with our db.
-    db.select('*')
-    .from('posts') //this is our table
+    // db.select('*')
+    db('posts') //this is our table
+    .select('title', 'contents')
     .then(posts => {
         res.status(200).json(posts);
     })
@@ -20,19 +21,59 @@ router.get('/', (req, res) => {
 });
 
 router.get('/:id', (req, res) => {
-
+    const { id } = req.params
+    db('posts')
+    .where({id})
+    .first() //picks the first element from the resulting array
+    .then(posts => {
+        res.status(200).json(posts);
+    })
+    .catch(err => {
+        res.json(err);
+    })
 });
 
 router.post('/', (req, res) => {
+//insert into pots () values ()
+    const postData =req.body;
+    //validate the data before inserting into db
+    db('posts')
+    .insert(postData, 'id') //by default this will return the id
+    .then(([id]) => {
+        db('posts')
+            .where({id})
+            .first() //picks the first element from the resulting array
+            .then(post => {
+                res.status(200).json(post);
+            })
+    })
+    .catch(err => {
+        res.json(err);
+    })
 
 });
 
 router.put('/:id', (req, res) => {
-
+    const changes = req.body;
+    db('posts').where('id', req.params.id).update(changes)
+    .then(count => {
+        res.status(200).json({message: `updated ${count} records`});
+    })
+    .catch( err => {
+        res.json(err);
+    })
 });
 
 router.delete('/:id', (req, res) => {
-
+    db('posts')
+    .where({id: req.params.id})
+    .del()
+    .then(count => {
+        res.status(200).json({message: `deleted ${count} records`});
+    })
+    .catch( err => {
+        res.json(err);
+    })
 });
 
 module.exports = router;
